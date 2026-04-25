e98c98d4ebc386fc49aad5ba2d4fdbc1059b4b9f
Siddhartha Routh <siddhartha.routh@sap.com>
2026-04-25 09:11:47 +0000

    Updated

---
+++ app/budgetanalyticsui/dist/Component.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/fe/core/AppComponent"],function(e){"use strict";return e.extend("budgetanalyticsui.Component",{metadata:{manifest:"json"}})});
+//# sourceMappingURL=Component.js.map
